13cbbd03e0d579a33488e2002aac06a6457195dc
Kumar Shivesh <116376181+kumarshivesh@users.noreply.github.com>
2024-02-13 22:00:27 +0530

    Added style.css in bmi-calculator folder

--- bmi-calculator/script.js
+++ bmi-calculator/script.js
@@ -9,12 +9,13 @@ form.addEventListener('submit', (event) => {
   event.preventDefault()
   const height = parseInt(document.querySelector('#height').value)
   const weight = parseInt(document.querySelector('#weight').value)
-  const results = document.querySelector('#results')
+  const displayBMI = document.querySelector('#display-bmi')
+  const displayCategory = document.querySelector('#display-category')
 
   if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please enter a valid height ${height}`
+    displayBMI.innerHTML = `Please enter a valid height ${height}`
   } else if (weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please enter a valid weight ${weight}`
+    displayBMI.innerHTML = `Please enter a valid weight ${weight}`
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     let category
@@ -22,11 +23,14 @@ form.addEventListener('submit', (event) => {
       category = 'Under Weight'
     } else if (bmi > 18.6 && bmi < 24.9) {
       category = 'Normal Range'
-    } else if (bmi > 24.9) {
+    } else if (bmi > 24.9 && bmi < 29.9) {
       category = 'Over Weight'
+    } else if (bmi > 30) {
+      category = 'Obesity'
     }
     //show the result
-    results.innerHTML = `<span>${bmi}</span> <br/> <span>${category}</span>`;
+    displayBMI.innerHTML = `${bmi}`;
+    displayCategory.innerHTML = `${category}`
   }
 })
 
